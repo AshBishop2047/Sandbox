f69f9c6e8403dfcef66e447d3ed90000f61ee773
AshBishop2047 <162354844+AshBishop2047@users.noreply.github.com>
2024-08-16 22:17:53 -0500

    a function to find how many milliseconds is in an amount of time

---
+++ codewars7/main.js
@@ -0,0 +1,5 @@
+function past(h, m, s){
+    return (h*3600000) + (m*60000) + (s*1000)
+    }
+    // find the value of milliseconds for each individual parameter;
+    // then add together each paramater's value and return the total 
