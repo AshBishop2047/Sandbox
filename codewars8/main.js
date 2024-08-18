f1e74a9aa88f754ad59439ba40cf982f278075cf
AshBishop2047 <162354844+AshBishop2047@users.noreply.github.com>
2024-08-17 21:38:32 -0500

    returns an array of numbers in a new array as negative numbers

---
+++ codewars8/main.js
@@ -0,0 +1,5 @@
+function invert(array) {
+    return array = array.map(num => -num);
+ }
+ // returns a new array via the map method
+ // turns it into a negative number by simply returning the number followed by a minus symbol
